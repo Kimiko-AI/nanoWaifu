321657c3283c9c9b4f0857d167c17b7743a629de
nruaif <nruaif@gmail.com>
2026-01-03 11:15:18 +0700

    Revert "Add class condition training"
    This reverts commit bd9d0567

--- train.py
+++ train.py
@@ -298,6 +298,7 @@ def train(config_path):
         optimizer.zero_grad()
         loss.backward()
 
+        # Add gradient clipping for stability and get norm for logging
         grad_norm = torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
 
         optimizer.step()
